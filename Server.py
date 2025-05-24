91e1acc7d55a32722f9615622715fcd14e5f2660
MattiDoubleU <luciernagas@gmx.de>
2025-05-24 15:51:05 +0100

    simple server

---
+++ Server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return "Hello Mamm"
+
+@app.route('/blah2')
+def blah():
+        return "this is blah2"
+
+if __name__ == "__main__":
+    app.run(debug = True)
